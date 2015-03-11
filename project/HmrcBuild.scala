27d3d0df83995f0fbdd166605972cc2b662b4d90
Charles Kubicek <charles@kubitech.com>
2015-03-11 17:04:37 +0000

    remove bintray dependencies

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -11,7 +11,7 @@ object HmrcBuild extends Build {
   import play.PlayImport._
 
   val nameApp = "play-health-fork"
-  val versionApp = "0.1.3"
+  val versionApp = "0.1.4"
 
   val appDependencies = Seq(
     "com.typesafe.play" %% "play" % PlayVersion.current,
